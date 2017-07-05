7e2ac0a08d3069345bea85449af5ad810796b90e
Augustus Chang <augustus.chang@rutgers.edu>
2017-07-05 01:13:53 -0700

    wrote static layout

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let dummyData = [{
+  embed_url : "https://giphy.com/embed/Yzk0KZhgcPdbW",
+  rating: "g",
+}, {
+  embed_url: "https://giphy.com/embed/d31xaEJuFKwdgmqc",
+  rating: "g",
+}, {
+  embed_url: "https://giphy.com/embed/138btjfXAn1xyo",
+  rating: "g",
+}, {
+  embed_url: "https://giphy.com/embed/CidndLIDBmYo0",
+  rating: "g",
+}, {
+  embed_url : "https://giphy.com/embed/Yzk0KZhgcPdbW",
+  rating: "g",
+}, {
+  embed_url: "https://giphy.com/embed/d31xaEJuFKwdgmqc",
+  rating: "g",
+}, {
+  embed_url: "https://giphy.com/embed/138btjfXAn1xyo",
+  rating: "g",
+}, {
+  embed_url: "https://giphy.com/embed/CidndLIDBmYo0",
+  rating: "g",
+}, {
+  embed_url: "https://giphy.com/embed/CidndLIDBmYo0",
+  rating: "g",
+}];
+
+class GifItem extends React.Component{
+  render(){
+    return (
+        <iframe src={this.props.url} width="270" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    );
+  }
+}
+
 class GifBoard extends React.Component{
   render(){
     return (
-      <p> I am a GifBoard </p>
+        <div>{dummyData.map(GifObj => <GifItem url={GifObj.embed_url} />)}</div>
     );
   }
 }
@@ -13,7 +50,9 @@ class SearchBox extends React.Component{
 
   render(){
     return (
-      <p> I am a search Box </p>
+      <form className="form-group">
+        <input type="text" placeholder="Search me!" className="form-control" /> <input className="btn btn-default form-control" type="submit" value="Search" />
+      </form>
     );
   }
 }
@@ -22,10 +61,20 @@ class GiphyApp extends React.Component{
 
   render(){
     return (
-      <div>
-        <SearchBox />
-        <GifBoard />
-      </div>
+      <div className="container-fluid">
+        <div className="row">
+        </div>
+        <div className="row" >
+          <div className="col-xs-4 col-xs-offset-4">
+            <SearchBox />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-8 col-xs-offset-2">
+          <GifBoard />
+          </div>
+        </div>
+     </div>
     );
   }
 
