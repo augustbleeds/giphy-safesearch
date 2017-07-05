6c94fc534c77b8351c0dc2f3b4f54af724c8d7e0
Augustus Chang <augustus.chang@rutgers.edu>
2017-07-04 19:12:56 -0700

    first three react components

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,7 +1,37 @@
-// var React = require('react');
-// var ReactDOM = require('react-dom');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>Inception of my new app!</p>,
+class GifBoard extends React.Component{
+  render(){
+    return (
+      <p> I am a GifBoard </p>
+    );
+  }
+}
+
+class SearchBox extends React.Component{
+
+  render(){
+    return (
+      <p> I am a search Box </p>
+    );
+  }
+}
+
+class GiphyApp extends React.Component{
+
+  render(){
+    return (
+      <div>
+        <SearchBox />
+        <GifBoard />
+      </div>
+    );
+  }
+
+}
+
+
+
+ReactDOM.render(<GiphyApp />,
    document.getElementById('root'));
