7f70b5a082cff7af46f49c3b4a1aaa234fa8c22e
Augustus Chang <augustus.chang@rutgers.edu>
2017-07-04 18:54:26 -0700

    add webpack

---
+++ reactApp/app.js
@@ -0,0 +1,7 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<p>React lives!</p>,
+   document.getElementById('root'));
